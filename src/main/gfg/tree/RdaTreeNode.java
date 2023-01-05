e69a3c88a023e56d345e03ae22b73f63d73ada79
amarnath-nitt <amarnath97.nitt@gmail.com>
2023-01-05 21:15:34 +0530

    created rda tree Node

---
+++ src/main/gfg/tree/RdaTreeNode.java
@@ -0,0 +1,13 @@
+package main.gfg.tree;
+
+public class RdaTreeNode {
+    public int data;
+    public RdaTreeNode left;
+    public RdaTreeNode right;
+
+    public RdaTreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
